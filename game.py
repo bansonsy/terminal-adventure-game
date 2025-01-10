54ed4e0f7ffe9895e129cb1e5d0740f6020f9d7d
bansonsy <bansonsy@gmail.com>
2025-01-09 17:16:36 -0800

    Second Commit

--- game.py
+++ game.py
@@ -5,5 +5,12 @@ def welcome():
     The village of Honeywood has been terrorized by strange, deadly creatures for months now. Unable to endure any 
     longer, the villagers pooled their wealth and hired the most skilled adventurer they could find: you. After
     listening to their tale of woe, you agree to enter the labyrinth where most of the creatures seem to originate,
-    and destroy the foul beasts. Armed with a longsword and a bundle of torches, you descend into the labyrinth, 
-    ready to do battle....""")
+    and destroy the foul beasts. Armed with a Long-sword and a bundle of torches, you descend into the labyrinth, 
+    ready to do battle....
+          
+    """)
+
+
+def play_game():
+    # Get player input
+    input("Press ENTER to continue...")
--- main.py
+++ main.py
@@ -5,6 +5,10 @@ def main():
     # Print a welcome screen
     game.welcome()
 
+    # Play the game until the game is over or til the player quits
+    while True:
+        game.play_game()
+
 
 if __name__ == "__main__":
     main()
