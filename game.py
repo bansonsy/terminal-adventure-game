0ca51abb3beb27e907ea6db00a5a72be880aa444
bansonsy <bansonsy@gmail.com>
2025-01-09 19:26:59 -0800

    Updated game.py + requirements.txt

--- game.py
+++ game.py
@@ -1,14 +1,22 @@
+from colorama import Fore
+
+
 # Welcome Text for Game
 def welcome():
-    print("                                                        DUNGEON")
-    print("""
-    The village of Honeywood has been terrorized by strange, deadly creatures for months now. Unable to endure any 
+    print(
+        Fore.RED
+        + "                                                       D U N G E O N"
+    )
+    print(
+        Fore.GREEN
+        + """
+    The village of Gotham has been terrorized by strange, deadly creatures for months now. Unable to endure any 
     longer, the villagers pooled their wealth and hired the most skilled adventurer they could find: you. After
     listening to their tale of woe, you agree to enter the labyrinth where most of the creatures seem to originate,
     and destroy the foul beasts. Armed with a Long-sword and a bundle of torches, you descend into the labyrinth, 
     ready to do battle....
-          
-    """)
+    """
+    )
 
 
 def play_game():
@@ -22,26 +30,60 @@ def play_game():
 
 def explore_labyrinth():
     while True:
-        player_input = input("-> ")
+        player_input = input(Fore.YELLOW + "-> ").lower().strip()
 
         # Do something with player's input
         if player_input == "help":
-            print("Show help goes here")
-
+            show_help()
         elif player_input == "quit":
             print("Overcome with terror, you flee the dungeon.")
             # TODO: Print out the final score
             play_again()
-
         else:
             print("I'm not sure what you mean... type 'help' for help.")
 
 
 def play_again():
-    # Yes/No user input
-    yn = input("Play Again? (Yes/No) -> ")
+    # Yes/No user input. Calls get_yn function.
+    yn = get_yn(Fore.YELLOW + "Do you want to play again? ")
+
     if yn == "yes":
         play_game()
     else:
         print("Until next time.")
         exit(0)
+
+
+def get_yn(question: str) -> str:
+    while True:
+        answer = input(question + "(Yes/No) -> ").lower().strip()
+
+        if answer not in ["yes", "no", "y", "n"]:
+            print("Please enter yes or no...")
+        else:
+            if answer == "y":
+                answer = "yes"
+            elif answer == "n":
+                answer = "no"
+            return answer
+
+
+def show_help():
+    print(
+        Fore.GREEN
+        + """Enter a command:
+          - N/S/E/W - Move in a direction
+          - Map - Show a map of the labyrinth
+          - Look - Look around and describe your environment
+          - Equip <item> - Use an item from your inventory
+          - Un-equip <item> - Stop using an item from your inventory
+          - Fight - Attack your enemy
+          - Examine <object> - Examine an object
+          - Get <item> - Pick up an item
+          - Drop <item> - Drop an item
+          - Rest - Restore your health
+          - Inventory - Show your inventory
+          - Status - Show current player status
+          - Quit - Quit the game
+    """
+    )
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import game
+from blessings import Terminal
 
 
 def main():
+    # Clear the terminal window
+    term = Terminal()
+    print(term.clear())
+
     # Play the game until the game is over or til the player quits
     game.play_game()
 
